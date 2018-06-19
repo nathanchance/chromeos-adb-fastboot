54da79fdd2cf17b8e5c4f37395ac4e11cb5c842c
Nathan Chancellor <natechancellor@gmail.com>
2018-06-19 12:28:21 -0700

    install.sh: Fix exit paths
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 # Move to user's download directory and make a temporary folder
 mkdir -p "${HOME}/Downloads/aftmp"
-cd "${HOME}/Downloads/aftmp" || ( echo "Temporary folder in Downloads could not be made!"; exit 1 )
+cd "${HOME}/Downloads/aftmp" || { echo "Temporary folder in Downloads could not be made!"; exit 1; }
 
 # Get the proper URL of the platform tools (the one below is just a redirect
 URL=$(curl -s https://dl.google.com/android/repository/platform-tools-latest-linux.zip | cut -d \" -f 2)
@@ -17,13 +17,13 @@ curl -s "${URL}" -o tmp.zip
 bsdtar -x -f tmp.zip
 
 # Install the tools
-cd platform-tools || ( echo "Unzipping platform-tools failed!"; exit 1 )
+cd platform-tools || { echo "Unzipping platform-tools failed!"; exit 1; }
 sudo mkdir -p /usr/local/bin /usr/local/lib64
 sudo install adb dmtracedump e2fsdroid etc1tool fastboot hprof-conv make_f2fs mke2fs mke2fs.conf sload_f2fs sqlite3 /usr/local/bin
 sudo install lib64/libc++.so /usr/local/lib64
 
 # Cleanup
-cd "${HOME}" || ( echo "HOME folder could not be found??"; exit 1 )
+cd "${HOME}" || { echo "HOME folder could not be found??"; exit 1; }
 rm -rf "${HOME}/Downloads/aftmp"
 
 # Get current version of adb-fastboot and install it
